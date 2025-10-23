27944e2f5db48d45ba4dc29db5d4ff09f2864ec4
CleberS_8 <127801630+Cleber-Sena@users.noreply.github.com>
2025-10-22 22:06:52 -0300

    Delete polymorphismpurefabrication/PadroesGRASPPolymPureFab/src/polymorphismPureFabrication/Notificacao.java

--- polymorphismpurefabrication/PadroesGRASPPolymPureFab/src/polymorphismPureFabrication/Notificacao.java
+++
@@ -1,7 +0,0 @@
-package polymorphismPureFabrication;
-
-//Notificacao.java (Interface)
-public interface Notificacao {
-//Este é o método que será implementado de formas diferentes (Polimorfismo)
-	void enviar(String destinatario, String mensagem);
-}
